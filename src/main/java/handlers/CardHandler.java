620393bd1d82e03cc93ef850dbab51f44b48b998
Haumidzaki <art684@yandex.ru>
2020-10-08 23:37:29 +0300

    smoll refactoring

--- src/main/java/handlers/CardHandler.java
+++ src/main/java/handlers/CardHandler.java
@@ -1,6 +1,5 @@
 package handlers;
 
-import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
