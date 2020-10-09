081a1ea9ebae9bff79d39a04680cc707fa2e46e1
Ju5tAlex <justalex777@mail.ru>
2020-10-09 12:49:35 +0300

    Added proper exit command for terminal

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,16 +7,24 @@ import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            System.out.println("Test");
+            System.out.println("Enter \"exit\" to shutdown server");
             Connection connection = ConnectionFromBd.getConnection();
             RunScript.execute(connection, new InputStreamReader(Main.class.getResourceAsStream("bd/initBD.sql")));
             RunScript.execute(connection, new InputStreamReader(Main.class.getResourceAsStream("bd/populateBD.sql")));
             HttpServerController.startServer();
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                if ((scanner.nextLine()).equalsIgnoreCase("exit")) {
+                HttpServerController.stopServer();
+                break;
+                }
+            }
         } catch (SQLException e){
             e.printStackTrace();
         }
